8a79bcf0b692ef90486502794d42585cc0d203e1
Paruru <139817163+origamidool@users.noreply.github.com>
2024-11-13 12:53:28 +0900

    Create Test.cs

---
+++ Assets/scripts/Test.cs
@@ -0,0 +1,10 @@
+namespace Atria
+{
+    public static class SongDataReader
+    {
+        public static void Deserialize<T>(string text, out T _out)
+        {
+            return null;
+        }
+    }
+}
